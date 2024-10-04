4f10d424266d0ba689a0bef4881a35077a6c3fdd
MTDau <50783337+MTDau@users.noreply.github.com>
2024-10-03 18:17:32 -0700

    Update 05Longest_Palindromic_Substring.cpp

--- 05Longest_Palindromic_Substring.cpp
+++ 05Longest_Palindromic_Substring.cpp
@@ -1,3 +1,55 @@
+#include <iostream>
+#include <string>
+
+class Solution
+{
+public:
+    std::string longestPalindrome(std::string s)
+    {
+
+        if (s.empty())
+        {
+            return ""; // empty string
+        }
+        result = s[0];      // In case of a single char is the answer
+        int start{}, end{}; // Keep track of the palindrome substr
+        for (int i = 0; i < s.size(); i++)
+        {
+            int event = expand(s, i, i);
+            int odd = expand(s, i, i + 1);
+            int len = std::max(event, odd);
+            if (len > end - start) // New longest
+            {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+                result = s.substr(start, len);
+            }
+        }
+        return result;
+    }
+
+private:
+    std::string result{};
+    int expand(std::string &s, int left, int right)
+    {
+        while (left >= 0 && right < s.size() && s[left] == s[right])
+        {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+};
+
+int main()
+{
+    Solution a;
+    std::cout << a.longestPalindrome("0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") << std::endl;
+    return 0;
+}
+
+/*
+Over runtime limit of leet code
 bool check_palindrom(std::string& s) {
     for (int left = 0, right = s.size() - 1; left < right; left++, right--) {
         if (s[left] != s[right]) {
@@ -24,3 +76,4 @@ public:
         return result;
     }
 };
+*/
