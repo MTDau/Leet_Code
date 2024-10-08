8ae91793e944776e5836b30c0957388ee733b807
MTDau <50783337+MTDau@users.noreply.github.com>
2024-10-08 16:29:12 -0700

    Update 08StringtoInteger(atoi).cpp

--- 08StringtoInteger(atoi).cpp
+++ 08StringtoInteger(atoi).cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        bool sign_before_digit{true};
+        bool have_sign{false};
         bool stop_flag{false};
         char sign{43};
-        long long result{};
+        long result{};
         for (char c : s) {
-            if (!stop_flag && sign_before_digit && (c == 45 || c == 43)) {
-                sign = c;
-                sign_before_digit = false;
+            if (c >= 48 && c <= 57) {
+                result = result * 10 + c - 48;
                 stop_flag = true;
-            } else if (c >= 48 && c <= 57) {
                 if (result > INT_MAX) {
                     if (sign == 45)
                         return INT_MIN;
                     return INT_MAX;
                 }
-                result = result * 10 + c - 48;
+            } else if (!stop_flag && !have_sign && (c == 45 || c == 43)) {
+                sign = c;
+                have_sign = true;
                 stop_flag = true;
             } else if (c == 32 && !stop_flag) {
                 continue;
@@ -24,7 +24,7 @@ public:
                 break;
             }
         }
-        if (sign == '-') {
+        if (sign == 45) {
             result = (-1) * result;
         }
         return result;
