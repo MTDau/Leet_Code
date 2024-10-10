e3ce0e66d18898e11e14b91882532bf6d5454b48
MTDau <50783337+MTDau@users.noreply.github.com>
2024-10-10 13:21:01 -0700

    Update 11ContainerWithMostWater.cpp

--- 11ContainerWithMostWater.cpp
+++ 11ContainerWithMostWater.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int max_area{0};
+        int leftptr = 0;
+        int rightptr = height.size() - 1;
+        while (leftptr < rightptr) {
+            int width = rightptr - leftptr;
+            int side = min(height[leftptr], height[rightptr]);
+            max_area =
+                max(max_area, width * side);
+            if (height[leftptr] > height[rightptr]) {
+                rightptr--;
+            } else {
+                leftptr++;
+            }
+        }
+        return max_area;
+    }
+};
