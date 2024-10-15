5bfdf5f670d1b1619dddce6047d9058462220d7e
MTDau <50783337+MTDau@users.noreply.github.com>
2024-10-14 18:03:58 -0700

    Update 15_3Sum.cpp

--- 15_3Sum.cpp
+++ 15_3Sum.cpp
@@ -1,3 +1,34 @@
+//Best solution that I can think of
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        std::vector<vector<int>> result;
+        for (int i{0}; i < nums.size() - 2; i++) {//Check for duplicate of the first number
+            if (i > 0 && nums.at(i) == nums.at(i - 1)) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+                if (total > 0)
+                    k--;
+                else if (total < 0)
+                    j++;
+                else {
+                    result.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    while (nums[j] == nums[j - 1] && j < k)//Check for duplicate of the second number. who the hell think that 0,0,0,0,0,0... is evil genius.
+                        j++;
+                }
+            }
+        }
+        return result;
+    }
+};
+
 //Not totally working with runtime problem
 class Solution {
 public:
