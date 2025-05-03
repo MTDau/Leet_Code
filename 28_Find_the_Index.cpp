8c3a2b58c2748369cf0a807a353e4c45ad514864
MTDau <50783337+MTDau@users.noreply.github.com>
2025-05-03 16:25:29 -0700

    Update 28_Find_the_Index.cpp

--- 28_Find_the_Index.cpp
+++ 28_Find_the_Index.cpp
@@ -5,3 +5,29 @@ public:
         return haystack.find(needle);
     }
 };
+
+//Logic run through the string
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int size1 = haystack.size();
+        int size2 = needle.size();
+        if (size2 > size1) // needle larger than haystack
+            return -1;
+
+        for (int i{}; i < size1; i++) {
+            for (int j{}; j <= size2; j++) {
+                if (j == size2) // run through the whole needle
+                    return i;
+                if (i + j == size1) // Not run through the whole needle but the
+                                    // end of haystack.
+                    return -1;
+                if (haystack[i + j] !=
+                    needle[j]) // Char in haystack match with char in needle
+                               // break the loop if not match
+                    break;
+            }
+        }
+        return -1;
+    }
+};
