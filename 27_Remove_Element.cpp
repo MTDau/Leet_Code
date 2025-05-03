113fbffff879720eb69d186bcb9e33cf41b9ff81
MTDau <50783337+MTDau@users.noreply.github.com>
2025-05-03 16:05:20 -0700

    Update 27_Remove_Element.cpp

--- 27_Remove_Element.cpp
+++ 27_Remove_Element.cpp
@@ -14,3 +14,18 @@ public:
     }
 };
 
+//Way easier to understand
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k{};
+        int size = nums.size();
+
+        for (int i{}; i < size; i++) {
+            if (nums[i] != val) {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
+};
