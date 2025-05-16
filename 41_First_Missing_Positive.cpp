2f1cbe29d3d169265b710de793cbb51deb03e26d
MTDau <50783337+MTDau@users.noreply.github.com>
2025-05-16 15:24:29 -0700

    Update 41_First_Missing_Positive.cpp

--- 41_First_Missing_Positive.cpp
+++ 41_First_Missing_Positive.cpp
@@ -6,13 +6,43 @@ class Solution
 public:
     int firstMissingPositive(std::vector<int> &nums)
     {
-
         std::sort(nums.begin(), nums.end());
         int target{1};
         for (int &n : nums)
         {
             if (n <= 0)
                 continue;
+            if (n == target)
+                target++;
+            else if (n > target)
+                return target;
+        }
+        return target;
+    }
+};
+
+
+
+
+
+#include <algorithm>
+#include <vector>
+
+class Solution
+{
+public:
+    int firstMissingPositive(std::vector<int> &nums)
+    {//This use more time to run compare to the original answer.
+        // Using lamda func to move all negative integer and 0 to end of vector
+        auto it = std::remove_if(nums.begin(), nums.end(),
+                                 [](int n)
+                                 { return n <= 0; });
+        // Erase the nums that we do not need to check.
+        nums.erase(it, nums.end());
+        std::sort(nums.begin(), nums.end());
+        int target{1};
+        for (int &n : nums)
+        {
             if (n == target)
                 target++;
             else if (n > target)
