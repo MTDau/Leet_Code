fb2f7b721f2ab3101c033e6032da74380aafe8c4
MTDau <50783337+MTDau@users.noreply.github.com>
2025-05-15 18:35:03 -0700

    Create 39_Combination_Sum.cpp

---
+++ 39_Combination_Sum.cpp
@@ -0,0 +1,54 @@
+//Not complete logic redo require.
+#include <vector>
+#include <algorithm>
+using std::vector;
+class Solution
+{
+public:
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
+        std::sort(candidates.begin(), candidates.end());
+
+        vector<vector<int>> result{};
+        for (int i{}; i < candidates.size(); i++)
+        {
+            if (candidates[0] > target)
+            {
+                break;
+            }
+            if (candidates[i] == target)
+            {
+                result.push_back({candidates[i]});
+                break;
+            }
+            int mul = target / candidates[i];
+            int fill = target % candidates[i];
+            if (fill == 0)
+            {
+                std::vector<int> semi(mul, candidates[i]);
+                result.push_back(semi);
+            }
+            else
+            {
+                for (int j{mul}; j > 0; j--) //Check for multiple copy of a candidate
+                {
+                    auto it = std::find(candidates.begin(), candidates.end(), fill);
+                    if (it != candidates.end())
+                    {
+                        std::vector<int> semi(j, candidates[i]);
+                        semi.push_back(fill);
+                        std::sort(semi.begin(), semi.end());
+                        result.push_back(semi);
+                        fill += candidates[i];
+                        continue;
+                    }
+                    fill += candidates[i];
+                }
+            }
+        }
+        std::sort(result.begin(), result.end());
+        auto it = std::unique(result.begin(), result.end());
+        result.erase(it,result.end());
+        return result;
+    }
+};
