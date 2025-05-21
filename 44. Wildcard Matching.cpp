064652931cb95e22b734cfd8db89ffe77d350cd3
MTDau <50783337+MTDau@users.noreply.github.com>
2025-05-20 20:08:35 -0700

    Update 44. Wildcard Matching.cpp

--- 44. Wildcard Matching.cpp
+++ 44. Wildcard Matching.cpp
@@ -1,3 +1,33 @@
+//Dynamic programing with truth table
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int s_len = s.length(), p_len = p.length();
+        int s_idx = 0, p_idx = 0;
+        int star_idx = -1, s_tmp_idx = -1;
+
+        while (s_idx < s_len) {
+            if (p_idx < p_len && (p[p_idx] == '?' || p[p_idx] == s[s_idx])) {
+                ++s_idx;
+                ++p_idx;
+            } else if (p_idx < p_len && p[p_idx] == '*') {
+                star_idx = p_idx;
+                s_tmp_idx = s_idx;
+                p_idx++;
+            } else if (star_idx == -1) {
+                return false;
+            } else {
+                p_idx = star_idx + 1;
+                s_idx = s_tmp_idx + 1;
+                s_tmp_idx = s_idx;
+            }
+        }
+
+        for (int i = p_idx; i < p_len; ++i) {
+            if (p[i] != '*')
+                return false;
+        }
+
 //Wrong answer since dce is count as a sequence too.
 #include <vector>
 #include <algorithm>
