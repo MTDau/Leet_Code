d6d637ea7c11ad00e5eaeef9ab3fec034235bc89
MTDau <50783337+MTDau@users.noreply.github.com>
2025-05-22 17:18:45 -0700

    Update 47. Permutations II.cpp

--- 47. Permutations II.cpp
+++ 47. Permutations II.cpp
@@ -1,3 +1,46 @@
+//Better update compare to the previous one check for swapable members
+#include <vector>
+#include <algorithm>
+#include <string>
+
+class Solution
+{
+public:
+    std::vector<std::vector<int>> permuteUnique(std::vector<int> &nums)
+    {
+        std::vector<std::vector<int>> ans{};
+        // Need sorting to check duplicated members
+        std::sort(nums.begin(), nums.end());
+        sol(nums, ans, 0);
+        return ans;
+    }
+    void sol(std::vector<int> &nums, std::vector<std::vector<int>> &ans, int idx)
+    {
+        // base case
+        if (idx == nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for (int i{idx}; i < nums.size(); i++)
+        {
+            bool swapable{true};
+            for (int j{idx}; j < i; j++)
+            {//Check for duplicate member to skip the swap.
+                if (nums[j] == nums[i])
+                {
+                    swapable = false;
+                    break;
+                }
+            }
+            if (!swapable)
+                continue;
+            std::swap(nums[idx], nums[i]);
+            sol(nums, ans, idx + 1);
+            std::swap(nums[idx], nums[i]); // back track;
+        }
+    }
+};
 //Lazy way to do this just update the old one
 #include <vector>
 #include <algorithm>
