844d2f0ae36c1572dfe76287ed643f5f8bc48234
MTDau <50783337+MTDau@users.noreply.github.com>
2025-05-24 20:42:40 -0700

    Create 51. N-Queens.cpp

---
+++ 51. N-Queens.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+class Solution
+{
+public:
+    std::vector<std::vector<std::string>> solveNQueens(int n)
+    {
+        std::vector<std::vector<std::string>> ans;
+        std::string s(n, '.');
+        std::vector<std::string> board(n,s);
+        solve(n, 0, board, ans);
+        return ans;
+    }
+    void solve(int n, int col, std::vector<std::string> &board, std::vector<std::vector<std::string>> &ans)
+    {
+        if (col == n)
+        {
+            ans.push_back(board);
+            return;
+        }
+        for (int row{}; row < n; row++)
+        {
+            if (safePlace(n, row, col, board))
+            {
+                board[row][col] = 'Q';
+                solve(n, col + 1, board, ans);
+                board[row][col] = '.'; // Back track
+            }
+        }
+    }
+
+    bool safePlace(int n, int row, int col, std::vector<std::string> board)
+    { // Only need to check the previous row because the lower is now filled.
+        for (int y{col}; y > -1; y--)
+        {
+            if (board[row][y] == 'Q')
+                return false;
+        }
+        for (int x{row}, y{col}; x > -1 && y > -1; x--, y--)
+        {
+            if (board[x][y] == 'Q')
+                return false;
+        }
+        for (int x{row}, y{col}; x < n && y > -1; x++, y--)
+        {
+            if (board[x][y] == 'Q')
+                return false;
+        }
+        return true;
+    }
+};
