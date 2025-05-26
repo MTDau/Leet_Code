1d25b49c7aadd5c848d54ac7a250af197e9ee03e
MTDau <50783337+MTDau@users.noreply.github.com>
2025-05-26 14:25:15 -0700

    Update 58. Length of Last Word.cpp

--- 58. Length of Last Word.cpp
+++ 58. Length of Last Word.cpp
@@ -22,3 +22,23 @@ public:
         return count;
     }
 };
+
+
+#include <vector>
+#include <algorithm>
+#include <cctype>
+#include <string>
+class Solution
+{
+public:
+    int lengthOfLastWord(std::string s)
+    {
+        int i = s.size() - 1;
+        while (i >= 0 && s[i] == ' ')
+            i--;
+        int ans{};
+        while (i >= 0 && s[i] != ' ')
+            ans++, i--;
+        return ans;
+    }
+};
