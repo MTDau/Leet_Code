fb72087ea422d1cd0760c523b4084d2e8b76b4ad
MTDau <50783337+MTDau@users.noreply.github.com>
2025-05-26 13:53:38 -0700

    Create 57. Insert Interval.cpp

---
+++ 57. Insert Interval.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <algorithm>
+using std::vector;
+
+class Solution
+{
+public:
+    vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int> &newInterval)
+    {
+        vector<vector<int>> ans{};
+        if (intervals.size() == 0)
+        {
+            ans.push_back(newInterval);
+            return ans;
+        }
+        std::vector<int> temp{newInterval};
+        bool merge{false}, single{true}, got_push{false};
+        int count{};
+        for (int i{0}; i < intervals.size(); i++)
+        {
+            if (intervals[i][1] >= newInterval[0] && intervals[i][1] <= newInterval[1] 
+                || intervals[i][0] <= newInterval[1] && intervals[i][0] >= newInterval[0] 
+                || intervals[i][0] <= newInterval[0] && intervals[i][1] >= newInterval[1])
+            {
+                temp = {std::min({intervals[i][0], newInterval[0], temp[0]}), std::max({intervals[i][1], newInterval[1], temp[1]})};
+                merge = false;
+                single = false;
+                got_push = false;
+            }
+            if (intervals[i][0] > newInterval[1] )
+            {
+                merge = true;
+                single = false;
+            }
+            if (merge && count == 0)
+            {
+                ans.push_back(temp);
+                merge = false;
+                got_push = true;
+                single = true;
+                count++;
+            }
+            if (single || count == 1)
+            {
+                ans.push_back(intervals[i]);
+            }
+        }
+        if (!got_push)
+            ans.push_back(temp);
+        return ans;
+    }
+};
