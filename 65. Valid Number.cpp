a5863e3ee2b9c73d7ad279c0fcf1cdf0e3d44e7f
MTDau <50783337+MTDau@users.noreply.github.com>
2025-05-30 16:22:24 -0700

    Create 65. Valid Number.cpp

---
+++ 65. Valid Number.cpp
@@ -0,0 +1,51 @@
+#include <cctype>
+#include <string>
+#include <iostream>
+
+class Solution
+{
+public:
+    bool isNumber(std::string s)
+    {
+        bool e_flag{false}, s_flag{false}, d_flag{false}, found_digit{false};
+        const int size = s.size();
+        for (int i{}; i < size; i++)
+        {
+            if (!std::isdigit(s[i]))
+            {
+                switch (s[i])
+                {
+                case '-':
+                case '+':
+                    if (s_flag || i == size - 1)
+                        return false;
+                    if (i != 0 && s[i - 1] != 'e' && s[i - 1] != 'E')
+                        return false;
+                    s_flag = true;
+                    break;
+                case 'e':
+                case 'E':
+                    if (e_flag || i == size - 1 || i == 0 || !found_digit)
+                        return false;
+                    e_flag = true;
+                    s_flag = false;
+                    break;
+                case '.':
+                    if (d_flag || e_flag || size == 1)
+                        return false;
+                    else if (i == 0 && !std::isdigit(s[i + 1]))
+                        return false;
+                    d_flag = true;
+                    break;
+                default:
+                    return false;
+                }
+            }
+            else
+                found_digit = true;
+        }
+        if (!found_digit)
+            return false;
+        return true;
+    }
+};
