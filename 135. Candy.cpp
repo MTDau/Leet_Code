f1b51a460b127a6df66fd17ee73d44c99c9b3086
MTDau <50783337+MTDau@users.noreply.github.com>
2025-06-02 12:08:38 -0700

    Update 135. Candy.cpp

--- 135. Candy.cpp
+++ 135. Candy.cpp
@@ -7,18 +7,19 @@ public:
     int candy(std::vector<int> &ratings)
     {
         int size = ratings.size();
-        std::vector<int> dp(size, 1); // Everyone have at least 1 candy
+        std::vector<int> candies(size, 1); // Everyone have at least 1 candy
         for (int i = 1; i < size; i++)
+        {
             if (ratings[i] > ratings[i - 1])
-                dp[i] = dp[i - 1] + 1;
+                candies[i] = candies[i - 1] + 1;
+        }
         int ans{};
         for (int i = size - 1; i > 0; i--)
         {
             if (ratings[i - 1] > ratings[i])
-                dp[i - 1] = std::max(dp[i - 1], dp[i] + 1);
-            ans += dp[i];
+                candies[i - 1] = std::max(candies[i - 1], (candies[i] + 1));
+            ans += candies[i];
         }
-        ans += dp[0];
-        return ans;
+        return ans += candies[0];
     }
 };
