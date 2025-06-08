2259ccf63072891ffecbe82ff71af2317a3b4854
MTDau <50783337+MTDau@users.noreply.github.com>
2025-06-07 18:22:16 -0700

    Update 128. Longest Consecutive Sequence.cpp

--- 128. Longest Consecutive Sequence.cpp
+++ 128. Longest Consecutive Sequence.cpp
@@ -21,13 +21,13 @@ public:
     }
 };
 
-//Run time error
+//should be O(n) some how take more time than sort.
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         std::unordered_set<int> set(nums.begin(), nums.end());
         int max{0};
-        for (int num : nums) {
+        for (int num : set) {
             if (set.find(num - 1) == set.end()) { // Begin of the sequence
                 int count{1};
                 while (set.find(num + count) != set.end())
@@ -38,3 +38,4 @@ public:
         return max;
     }
 };
+
