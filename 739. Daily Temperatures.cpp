17e1dcc4ef34dec61feecb253576945e74964522
MTDau <50783337+MTDau@users.noreply.github.com>
2025-06-19 10:25:20 -0700

    Create 739. Daily Temperatures.cpp

---
+++ 739. Daily Temperatures.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+class Solution
+{
+public:
+    std::vector<int> dailyTemperatures(std::vector<int> &temperatures)
+    {
+        int size{temperatures.size()};
+        std::vector<int> ans(size, 0);
+
+        for (int i{size - 2}; i > -1; i--)
+        {
+            int j = i + 1;
+            while (j < size && temperatures[j] <= temperatures[i])
+            {
+                if (ans[j] == 0) // There is not a day after this that have higher temp
+                {
+                    j = size;//Make j out of bound.
+                    break;
+                }
+                j += ans[j]; // Jump to the day with higher temperature
+            }
+            if (j < size)
+                ans[i] = j - i;
+        }
+        return ans;
+    }
+};
